0a11f2d30c67881ca0f83c0ff78f9d11f02ae0a2
Jackson <jackson.cutler@gmail.com>
2024-12-05 18:52:47 -0800

    Adjust styles for light and dark themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import { useState } from 'react';
 import { initFirebase } from './util/firebase';
 import AudioInterface from './components/AudioInterface';
 import './App.css';
 
 function App() {
+  const [theme, setTheme] = useState('light');
+
+  function toggleTheme() {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  }
+
   initFirebase();
-  let theme = 'light';
   return (
     <main data-theme={theme}>
       <AudioInterface />
+      <button className='testbtn' onClick={toggleTheme}>Theme</button>
     </main>
   );
 }
